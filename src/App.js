bb54e1f94e8504341095d7a0bb6618b9a1abfcd7
vamshi8267 <vamshigajangi009@gmail.com>
2024-04-01 11:07:16 +0000

    tech-era

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {Switch, Route, Redirect} from 'react-router-dom'
+import Home from './components/Home'
+import CourseItemDetails from './components/CourseItemDetails'
+
+import './App.css'
+import NotFound from './components/NotFound'
+
+// Replace your code here
+const App = () => (
+  <div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/courser/:id" component={CourseItemDetails} />
+      <Route exact path="/not-found" component={NotFound} />
+      <Redirect to="/not-found" />
+    </Switch>
+  </div>
+)
+
+export default App
---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import './index.css'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <div>
+          <Link to="/">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/tech-era/website-logo-img.png"
+              alt="website logo"
+              className="headerLogo"
+            />
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
